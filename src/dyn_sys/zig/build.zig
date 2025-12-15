3b92f92bf41f52af09bf8e37e89a7b09230d36c0
gmono <gaozijian0411@foxmail.com>
2025-12-16 02:23:46 +0800

    改进zig build

--- src/dyn_sys/zig/build.zig
+++ src/dyn_sys/zig/build.zig
@@ -1,24 +1,52 @@
 const std = @import("std");
 
 pub fn build(b: *std.Build) void {
-    const target = b.standardTargetOptions(.{});
-    const optimize = b.standardOptimizeOption(.{});
+    const target =
+        if (@hasDecl(std.Build, "standardTargetOptions"))
+            b.standardTargetOptions(.{})
+        else
+            b.standardTargetOptions(.{});
+    const optimize =
+        if (@hasDecl(std.Build, "standardOptimizeOptions"))
+            b.standardOptimizeOptions(.{})
+        else
+            b.standardOptimizeOption(.{});
 
-    const lib = b.addStaticLibrary(.{
-        .name = "dyn_sys_zig",
-        .root_source_file = b.path("src/lib.zig"),
-        .target = target,
-        .optimize = optimize,
-    });
-    b.installArtifact(lib);
+    if (@hasDecl(std.Build, "addLibrary")) {
+        const mod = b.createModule(.{
+            .root_source_file = b.path("src/lib.zig"),
+            .target = target,
+            .optimize = optimize,
+        });
+        const lib = b.addLibrary(.{
+            .name = "dyn_sys_zig",
+            .root_module = mod,
+            .linkage = .static,
+        });
+        b.installArtifact(lib);
 
-    const dll = b.addSharedLibrary(.{
-        .name = "dyn_sys_zig_dll",
-        .root_source_file = b.path("src/lib.zig"),
-        .target = target,
-        .optimize = optimize,
-    });
-    b.installArtifact(dll);
+        const dll = b.addLibrary(.{
+            .name = "dyn_sys_zig_dll",
+            .root_module = mod,
+            .linkage = .dynamic,
+        });
+        b.installArtifact(dll);
+    } else {
+        const lib = b.addStaticLibrary(.{
+            .name = "dyn_sys_zig",
+            .root_source_file = b.path("src/lib.zig"),
+            .target = target,
+            .optimize = optimize,
+        });
+        b.installArtifact(lib);
+        const dll = b.addSharedLibrary(.{
+            .name = "dyn_sys_zig_dll",
+            .root_source_file = b.path("src/lib.zig"),
+            .target = target,
+            .optimize = optimize,
+        });
+        b.installArtifact(dll);
+    }
 
     const unit_tests = b.addTest(.{
         .root_source_file = b.path("src/lib.zig"),
